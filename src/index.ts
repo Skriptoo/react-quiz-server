878c3d3c0178986625298930fd7c978ece6fe2f3
Kamil <72623198+Skriptoo@users.noreply.github.com>
2023-12-13 00:10:58 +0100

    added connection socket

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,47 @@
 import express, {Express, Request, Response} from "express";
-import { Socket } from "socket.io";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import cors from 'cors';
 
-const port = 8000;
+const PORT = process.env.PORT || 8000;
 
-const app: Express = express()
+const app: Express = express();
+
+const expressServer = app.listen(PORT, ()=> {
+    console.log(`now listening on port ${PORT}`);
+});
+
+app.use(cors({
+    origin: "*"
+}));
+
+app.use(express.json());
+const httpServer = createServer();
+
+const io = new Server(expressServer, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log(`: ${socket.id} connected`)
+    socket.on('disconnect', () => {
+        console.log("user disconnected")
+    });
+    //socket.emit('connect', {message: 'user connected'})
+});
 
 
 app.get("/", (req: Request, res: Response) => {
     res.send("essa");
+    io.emit('connection');
 })
 
 
 app.get("/hi", (req, res) => {
+    io.emit('connection');
     res.send("hi");
-})
+});
 
-
-app.listen(port, () => {
-    console.log(`now listening on port ${port}`)
-})
--- src/socket.ts
+++
@@ -1,4 +0,0 @@
-import {Server as HTTPServer} from 'http';
-import { Socket, Server } from "socket.io";
-
-//essa
